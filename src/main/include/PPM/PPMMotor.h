be8f534e8ce8266620cabf8bf839009948395ccc
CJBuchel <connorbuchel@gmail.com>
2023-07-31 13:44:17 +0800

    Whoops, forgot how slow 100Hz, switched to 1kHz for PPM signal update

--- src/main/include/PPM/PPMMotor.h
+++ src/main/include/PPM/PPMMotor.h
@@ -15,7 +15,7 @@ class PPMMotor : private PPMController {
    * 
    * @param channel 
    */
-  explicit PPMMotor(int channel) : PPMController(channel, 100_Hz) {}
+  explicit PPMMotor(int channel) : PPMController(channel, 1_kHz) {}
 
   /**
    * @brief Construct a new PPMMotor object with update rate (default is 100hz)
