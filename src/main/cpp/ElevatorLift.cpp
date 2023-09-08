8f993432afa416d925f9efedc6bef1fea521c15b
Vyppal <actoplie@gmail.com>
2023-09-08 17:37:52 +0800

    made a new elevator system to have controls and limits based on high currents. High current threshold yet to be tested to identify

--- src/main/cpp/ElevatorLift.cpp
+++ src/main/cpp/ElevatorLift.cpp
@@ -3,15 +3,35 @@
 
 ElevatorLift::ElevatorLift(ElevatorConfig *config) : _config(config) {}
 
-void ElevatorLift::setTarget(double target) {
-  _target = target;
-}
+// current
+// voltage
+// encoder value (rotational velocity)
 
-void ElevatorLift::onUpdate(double deltaTime, double target) {
-  //             = (2 * pi * r) * pulleyRatio * motorPosition
-  // _currentHeight = (2 * 3.14159265359 * 0.037) * pulleyRatio * (motor.getTicks() / 42);
-  double trueTarget = (target + 1) / 2;
-  _target = (_config->maxHeight - _config->minHeight) * trueTarget + _config->minHeight;
-  _config->motor.set(_config->pidController.compute(_target, _currentHeight, deltaTime));
+
+void ElevatorLift::onUpdate(double deltaTime, double speedTarget, int directionRequest) {
+  _currentTick = _config->motor.getEncoderValue();
+  if (directionRequest == 0) {
+    _prevTick = _currentTick;
+    _requestedSpeed = 0;
+    halt();
+    return;
+  }
+  _requestedSpeed = (speedTarget + 1) / 2 * maxElevatorSpeed * directionRequest; // snapping range of -1->1 to 0->1 then multiplying by max elevator speed then by direction
+  _currentSpeed = (2 * 3.14159265359 * 0.037) * pulleyRatio * ((_currentTick - _prevTick) * deltaTime / 42);
+  
+  unnamedFunction(directionRequest);
+  
+  _config->motor.set(_config->pidController.compute(_requestedSpeed, _currentSpeed, deltaTime));
+  _prevTick = _currentTick;
 }
 void ElevatorLift::halt() { _config->motor.set(0); }
+
+void ElevatorLift::unnamedFunction1(int directionRequest) {
+  // ensures that elevator doesn't want to keep going up or down if too high or low (it checks its height based on abnormally high current values that last a duration of time)
+  if (directionRequest == 0) {  return;  }
+  if (_config->motor.getCurrent() < currentWarningThreshold) {  limitedDirection = 0; return;  }
+  warningCounter++;
+  if (warningCounter < warningCounterThreshold) {  limitedDirection = 0; return;  }
+  warningCounter = 0;
+  limitedDirection = directionRequest;
+}
--- src/main/include/ElevatorLift.h
+++ src/main/include/ElevatorLift.h
@@ -23,17 +23,31 @@ class ElevatorLift {
  public:
   ElevatorLift(ElevatorConfig *_config);
 
-  void setTarget(double target);
   void onUpdate(double deltaTime, double target);
+  void unnamedFunction1();
 
   void halt();
 
  private:
   ElevatorConfig *_config;
 
-  double _target;
-  double _currentHeight;
+  double _requestedSpeed;
+  // double _currentHeight;
+
+  double _currentSpeed;
+
+  double maxElevatorSpeed = 3; // m/s
 
   double pulleyRatio = 16 / 42;
+
+  int _prevTick = 0;
+  int _currentTick = 0;
+
+
+  double currentWarningThreshold = 1500; // needs testing
+  int warningCounter = 0;
+  int warningCounterThreshold = 25; // around 0.5 seconds (10 at 50Hz)
+
+  int limitedDirection = 0; // -1 if limit down, 1 if limit up, 0 if no limit
 
 };
