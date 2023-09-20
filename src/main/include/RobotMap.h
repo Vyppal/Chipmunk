2a2063015f73f5ac9847b0975829eec600b25e5c
Vyppal <actoplie@gmail.com>
2023-09-20 17:04:24 +0800

    a

--- src/main/include/RobotMap.h
+++ src/main/include/RobotMap.h
@@ -17,38 +17,12 @@ struct RobotMap {
     frc::Joystick primary{0};
   };
   Controllers controllers;
-  // struct TankConfiguration {
-  //   PPMFlipsky leftFront{0};
-  //   PPMFlipsky leftBack{1};
-  //   PPMFlipsky rightFront{2};
-  //   PPMFlipsky rightBack{3};
-  //   int wheelDist = 0.64; // distance between wheel sides
-
-  //   // LeftMotors {
-  //   //   PPMFlipsky front{0};
-  //   //   PPMFlipsky back{1};
-  //   // }; LeftMotors leftMotors;
-  //   // RightMotors {
-  //   //   PPMFlipsky front{2};
-  //   //   PPMFlipsky back{3};
-  //   // }; RightMotors rightMotors;
-    
-  //   TankConfig {
-  //     leftFront,
-  //     leftBack,
-  //     rightFront,
-  //     rightBack,
-  //     wheelDist
-  //   };
-  // };
-  // TankConfiguration tankConfig;
-
   TankConfig tankConfig{
     PPMFlipsky{0},
+    PPMFlipsky{9},
     PPMFlipsky{1},
-    PPMFlipsky{2},
-    PPMFlipsky{3},
+    PPMFlipsky{8},
     0.64
   };
-  PPMFlipsky elevatorMotor{4};
+  PPMFlipsky elevatorMotor{5};
 };
